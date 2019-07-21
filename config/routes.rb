a4fbfd8d9ac103ddf9a645fead63ede1a615eff8
Oblum1989 <oscar.blum@gmail.com>
2019-07-21 18:33:45 -0500

    se modifican las vistas index y show de los pedidos agregando bosquejo inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'orders/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders
+
+  root 'orders#index'
 end
