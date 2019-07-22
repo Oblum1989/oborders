cf63bef12bb843c526ed11925d17f80e8a915615
Oblum1989 <oscar.blum@gmail.com>
2019-07-21 19:47:10 -0500

    creacion de controlador para los clientes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/show'
+  get 'customers/new'
+  get 'customers/edit'
   devise_for :users
   resources :orders
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_235856) do
+ActiveRecord::Schema.define(version: 2019_07_22_002318) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "addres"
+    t.integer "celphone"
+    t.integer "nit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
