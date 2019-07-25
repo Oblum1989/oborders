10527ea0780072be8aff786ac0cb7248df127831
Oblum1989 <oscar.blum@gmail.com>
2019-07-24 19:19:59 -0500

    se crean los metodos total y subtotal

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,15 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products
   has_many :products, through: :order_products, dependent: :destroy
+
+  def total
+		details = self.order_products
+
+		total = 0.0
+		details.flat_map do |d|
+			total += d.quantity * d.product.price
+		end
+		total
+	end
+
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,9 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  def subtotal
+		subtotal = self.quantity * self.product.price
+	end
+
 end
